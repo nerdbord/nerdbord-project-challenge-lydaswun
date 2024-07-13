30fa1d2cb765f8da715e36cd565f953333680c98
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-07-14 00:12:06 +0200

    feat: implement sanity client

---
+++ src/app/lib/sanity.client.ts
@@ -0,0 +1,28 @@
+﻿import "server-only";
+
+import { createClient, type QueryParams } from "next-sanity";
+
+export const sanityClient = createClient({
+	projectId: "dr96nfh9",
+	dataset: "production",
+	apiVersion: "2022-03-07",
+	useCdn: false,
+	perspective: "previewDrafts",
+});
+
+export async function sanityFetch<QueryResponse>({
+	query,
+	params = {},
+	tags,
+}: {
+	query: string;
+	params?: QueryParams;
+	tags?: string[];
+}) {
+	return sanityClient.fetch<QueryResponse>(query, params, {
+		next: {
+			revalidate: process.env.NODE_ENV === "development" ? 30 : 3600,
+			tags,
+		},
+	});
+}
