7aac47d81b859a3c79fd209695ff4b2a2c105779
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-07-15 15:39:29 +0200

    feat: display post content on post page

--- src/components/SinglePost/SinglePost.tsx
+++ src/components/SinglePost/SinglePost.tsx
@@ -2,64 +2,60 @@
 import React from "react";
 import Image from "next/image";
 import { IoIosHeartEmpty, IoMdPaperPlane } from "react-icons/io";
+import { PortableText } from "next-sanity";
 import { Button } from "../Atoms/Button/Button";
 import { StatsDisplay } from "../Atoms/Stats/Stats";
 import styles from "./SinglePost.module.css";
 import Arrow from "@/assets/Arrow";
-import nopicture from "@/assets/nopicture.png";
-
-type Author = {
-	name: string;
-	image: string;
-};
-
-type Post = {
-	id: number;
-	imageSrc?: string;
-	imageAlt: string;
-	category: string;
-	publicationDate: string;
-	likes?: number;
-	shares?: number;
-	title: string;
-	content: string;
-	imgURL?: string;
-	author: Author;
-};
+import noPicture from "@/assets/nopicture.png";
+import { type PostDetailedType } from "@/app/lib/sanity.types";
+import { urlForImage } from "@/app/lib/sanity.image";
+import { formatDate } from "@/utils/formatDate";
 
 type PostDetailProps = {
-	post: Post;
+	post: PostDetailedType;
 };
 
 export const SinglePost: React.FC<PostDetailProps> = ({ post }) => {
-	const { likes = 20, shares = 40 } = post;
+	const { author, categories, content, mainImage, publishedAt, title } = post;
+
+	const imgURL = urlForImage(mainImage.image).url();
+	const postPublishedAt = formatDate(publishedAt);
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.header}>
 				<div className={styles.imagebox}>
-					<Image src={post.imgURL || nopicture} alt={post.imageAlt} width={500} height={300} />
+					<Image src={imgURL || noPicture} alt={mainImage.alt} width={500} height={300} />
 				</div>
 				<div className={styles.titlebox}>
-					<h1 className={styles.title}>{post.title}</h1>
+					<h1 className={styles.title}>{title}</h1>
 					<ul className={styles.postdata}>
 						<li className={styles.item}>
 							<p>Category</p>
-							<p className={styles.subtitle}>{post.category}</p>
+							{categories.length > 0 && (
+								<div className={styles.categories}>
+									{categories.map((category) => (
+										<p key={category._id} className={styles.subtitle}>
+											{category.name}
+										</p>
+									))}
+								</div>
+							)}
 						</li>
 						<li className={styles.item}>
 							<p>Publication Date</p>
-							<p className={styles.subtitle}>{post.publicationDate}</p>
+							<p className={styles.subtitle}>{postPublishedAt}</p>
 						</li>
 						<li className={styles.item}>
 							<p>Author</p>
-							<p className={styles.subtitle}>{post.author.name}</p>
+							<p className={styles.subtitle}>{author.name}</p>
 						</li>
 					</ul>
 					<div className={styles.buttonbox}>
 						<div className={styles.social}>
-							<StatsDisplay count={likes} icon={IoIosHeartEmpty} label={""} />
-							<StatsDisplay count={shares} icon={IoMdPaperPlane} label={""} />
+							<StatsDisplay count={20} icon={IoIosHeartEmpty} label={""} />
+							<StatsDisplay count={50} icon={IoMdPaperPlane} label={""} />
 						</div>
 						<Button
 							text="Like"
@@ -72,7 +68,7 @@ export const SinglePost: React.FC<PostDetailProps> = ({ post }) => {
 					</div>
 				</div>
 			</div>
-			<p className={styles.content}>{post.content}</p>
+			<PortableText value={content} />
 		</div>
 	);
 };
