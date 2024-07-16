78c91671f5a698a3edd94f209c89e6288af41e1e
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-07-16 20:23:32 +0200

    refactor: refactor queries

--- src/app/lib/sanity.queries.ts
+++ src/app/lib/sanity.queries.ts
@@ -1,9 +1,23 @@
 ﻿import { groq } from "next-sanity";
 
-export const POST_BY_SLUG_QUERY = groq`*[_type == "post" && slug.current == $slug][0]{
+const postCardFields = groq`
     _id,
     title,
     "slug": slug.current,
+    "mainImage": {
+        "image": mainImage,
+        "alt": mainImage.alt
+    },
+    "categories": categories[]->{
+        _id,
+        name
+    },
+    likes,
+    visitors
+    `;
+
+export const POST_BY_SLUG_QUERY = groq`*[_type == "post" && slug.current == $slug][0]{
+    ${postCardFields},
     "author": {
         "name": author->name,
         "authorAvatar": {
@@ -11,56 +25,17 @@ export const POST_BY_SLUG_QUERY = groq`*[_type == "post" && slug.current == $slu
             "alt": author->authorAvatar.alt
         }
     },
-    "mainImage": {
-        "image": mainImage,
-        "alt": mainImage.alt
-    },
-    "categories": categories[]->{
-        _id,
-        name
-    },
     publishedAt,
-    preview,
     content,
-    likes,
-    visitors
   }`;
 
 export const ALL_POSTS_QUERY = groq`*[_type == "post" && popular == null] | order(publishedAt desc){
-    _id,
-    title,
-    "slug": slug.current,
-    "author": author->{name},
-    "mainImage": {
-        "image": mainImage,
-        "alt": mainImage.alt
-    },
-    "categories": categories[]->{
-        _id,
-        name
-    },
-    preview,
-    publishedAt,
-    likes,
-    visitors
+    ${postCardFields}
   }`;
 
 export const POPULAR_POST_QUERY = groq`*[_type == "post" && popular == true][0]{
-    _id,
-    title,
-    "slug": slug.current,
+    ${postCardFields},
     "author": author->{name},
-    "mainImage": {
-        "image": mainImage,
-        "alt": mainImage.alt
-    },
-    "categories": categories[]->{
-        _id,
-        name
-    },
     preview,
     publishedAt,
-    popular,
-    likes,
-    visitors
   }`;
