eb66e44371a51b090830271c78b8b3b78d892bab
wawrzonnnn <norbert.wawrzonek@gmail.com>
2024-07-17 22:16:31 +0200

    fix: resolve cnoflicts

--- src/app/lib/sanity.queries.ts
+++ src/app/lib/sanity.queries.ts
@@ -1,40 +1,64 @@
 ﻿import { groq } from "next-sanity";
-const postCardFields = groq`
-    _id,
-    title,
-    "slug": slug.current,
-    "mainImage": {
-        "image": mainImage,
-        "alt": mainImage.alt
-    },
-    "categories": categories[]->{
-        _id,
-        name
-    },
-    likes,
-    visitors
-    `;
 
 export const POST_BY_SLUG_QUERY = groq`*[_type == "post" && slug.current == $slug][0]{
-    ${postCardFields},
-    "author": {
-        "name": author->name,
-        "authorAvatar": {
-            "image": author->authorAvatar,
-            "alt": author->authorAvatar.alt
-        }
-    },
-    publishedAt,
-    content,
-  }`;
+  _id,
+  title,
+  "slug": slug.current,
+  "author": {
+    "name": author->name,
+    "authorAvatar": {
+      "image": author->authorAvatar.asset->url,
+      "alt": author->authorAvatar.alt
+    }
+  },
+  "mainImage": {
+    "image": mainImage.asset->url,
+    "alt": mainImage.alt
+  },
+  "categories": categories[]->{
+    _id,
+    name
+  },
+  publishedAt,
+  preview,
+  content
+}`;
 
 export const ALL_POSTS_QUERY = groq`*[_type == "post" && popular == null] | order(publishedAt desc){
-    ${postCardFields}
-  }`;
+  _id,
+  title,
+  "slug": {
+    "current": slug.current
+  },
+  "author": author->{name},
+  "mainImage": {
+    "image": mainImage.asset->url,
+    "alt": mainImage.alt
+  },
+  "categories": categories[]->{
+    _id,
+    name
+  },
+  preview,
+  publishedAt,
+}`;
 
 export const POPULAR_POST_QUERY = groq`*[_type == "post" && popular == true][0]{
-    ${postCardFields},
-    "author": author->{name},
-    preview,
-    publishedAt,
-  }`;
+  _id,
+  title,
+  "slug": {
+    "current": slug.current
+  },
+  "author": author->{name},
+  "mainImage": {
+    "image": mainImage.asset->url,
+    "alt": mainImage.alt
+  },
+  "categories": categories[]->{
+    _id,
+    name
+  },
+  preview,
+  publishedAt,
+  popular
+}`;
